d25dd0af7572edda84566248e85df11c18935b3a
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-02-05 22:04:45 -0500

    add bash scripts to start/stop all services

---
+++ run-all.sh
@@ -0,0 +1,18 @@
+ #!/bin/sh
+ cd /www/packs-services/app/
+ grunt serve &
+
+ cd /www/packs-services/auth/
+ npm start &
+
+ cd /www/packs-services/game/
+ npm start &
+
+ cd /www/packs-services/matchmaker/
+ npm start &
+
+ cd /www/packs-services/queues/
+ node matchmaking.js &
+
+ cd /www/packs-services/socket
+ npm start &
